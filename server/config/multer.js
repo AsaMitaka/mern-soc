870408cd5b753e3ee7b233603beab7f1795c9c76
AsaMitaka <cepelits@gmail.com>
2024-01-05 22:00:45 +0200

    + multer config

---
+++ server/config/multer.js
@@ -0,0 +1,28 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1000);
+    cb(null, uniqueSuffix + '_' + file.originalname);
+  },
+});
+
+function fileFilter(req, file, cb) {
+  const mimeTypes = ['image/png', 'image/jpeg', 'image/gif'];
+  if (!mimeTypes.includes(file.mimetype)) {
+    const error = new Error('Invalid file type: ');
+    cb(error, false);
+  }
+  cb(null, true);
+}
+
+const upload = multer({
+  fileFilter,
+  limits: { fileSize: 3 * 1024 * 1024 }, // 3145728 3 mb
+  storage,
+});
+
+module.exports = upload;
