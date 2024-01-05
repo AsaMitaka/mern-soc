c77a5b906eb6e2fbaad10803c791eac85204a226
AsaMitaka <cepelits@gmail.com>
2024-01-05 22:01:01 +0200

    + main server file

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const route = require('./routes/');
+require('dotenv').config();
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', route);
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB).then(() => {
+      console.log('Connected to MongoDB');
+    });
+
+    app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+start();
