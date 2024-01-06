30b7189fd88a0bd75f0376cf5d0469939ae7a095
AsaMitaka <cepelits@gmail.com>
2024-01-06 21:18:21 +0200

    * minor

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,22 +1,9 @@
-import Button from '../components/ui/button';
-import Input from '../components/ui/input';
-
 const Home = () => {
   return (
-    <div>
+    <>
       <h1>Home</h1>
-      <Button>Primary</Button>
-      <Button isLarge>isLarge Primary</Button>
-      <Button isLarge variant="secondary">
-        isLarge Primary
-      </Button>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <Input placeholder="text" />
-        <Input placeholder="text" isLarge />
-        <Input placeholder="text" />
-        <Input variant="secondary" placeholder="text" isLarge />
-      </div>
-    </div>
+      <h3>Center</h3>
+    </>
   );
 };
 
