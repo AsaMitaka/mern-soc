4ea6e2988ccbaf4adddb586d99aa334d992c1871
AsaMitaka <cepelits@gmail.com>
2024-01-06 21:16:03 +0200

    + tailwind

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { stylexPlugin } from 'vite-plugin-stylex-dev';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), stylexPlugin()],
+  plugins: [react()],
 });
