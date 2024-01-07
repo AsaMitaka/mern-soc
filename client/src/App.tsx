3023974ba45e0dec8980c8cf2f926963e4e3f910
AsaMitaka <cepelits@gmail.com>
2024-01-08 00:11:39 +0200

    * fix and added new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
-import { About, Home, Login, Register, Profile } from './pages';
+import { Search, Home, Login, Register, ProfileLikes, ProfilePosts, About, Error } from './pages';
 import PrivateRoute from './utils/privateroute';
+import ProfileLayout from './components/profileLayout';
 
 const App = () => {
   return (
@@ -11,7 +12,12 @@ const App = () => {
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/search?" element={<Search />} />
+          <Route element={<ProfileLayout />}>
+            <Route path="/profile/:id" element={<ProfilePosts />} />
+            <Route path="/profile/:id/likes" element={<ProfileLikes />} />
+          </Route>
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </>
