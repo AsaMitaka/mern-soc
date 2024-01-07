bb8d2db5ec30474f0111a2b313730811e15f115e
AsaMitaka <cepelits@gmail.com>
2024-01-08 00:13:40 +0200

    * added github link

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
   return (
     <>
       <h1>About</h1>
+      <h3>Pet project</h3>
+      <Link target="_blank" to="github.com/asamitaka">
+        Github
+      </Link>
     </>
   );
 };
