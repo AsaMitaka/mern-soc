9aba82b99c0f270487a279dc10dee8f0cb0e6164
AsaMitaka <cepelits@gmail.com>
2024-01-08 00:10:59 +0200

    * add new exports

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,5 @@
 export { default as Header } from './header';
+export { default as Post } from './post';
+export { default as profileLayout } from './profileLayout';
+export { default as Trends } from './trends';
+// export { default as  } from './';
