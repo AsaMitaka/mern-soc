58f0d547adb194075c999c8e05d5a1e30b141a78
AsaMitaka <cepelits@gmail.com>
2024-01-07 03:12:15 +0200

    * updated routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { About, Home, Login, Register } from './pages';
+import { About, Home, Login, Register, Profile } from './pages';
 import PrivateRoute from './utils/privateroute';
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/profile/:id" element={<Profile />} />
         </Route>
       </Routes>
     </>
