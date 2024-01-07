4ed1dc209154d4864206af684b549bfad8094aa2
AsaMitaka <cepelits@gmail.com>
2024-01-08 00:14:26 +0200

    * added new exports

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,8 @@
 export { default as About } from './about';
+export { default as Error } from './error';
 export { default as Home } from './home';
 export { default as Login } from './login';
 export { default as Register } from './register';
-export { default as Profile } from './profile';
+export { default as ProfileLikes } from './profileLikes';
+export { default as ProfilePosts } from './profilePosts';
+export { default as Search } from './search';
