31c03d65bdfc0fe724d350eef358bbe50ad9f3ed
AsaMitaka <cepelits@gmail.com>
2024-01-07 03:11:40 +0200

    * minor styles fix

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const Btn = ({ isBig, isPrimary, isRounded, title }: ButtonProps) => {
     <button
       className={`
       my-2
-      ${isBig ? 'px-6 py-2 text-lg border-2' : 'px-4 py-2 text-md border-[1px]'}
+      ${isBig ? 'px-6 py-2 text-lg border-2 w-full' : 'w-fit px-4 py-2 text-md border-[1px]'}
       ${isRounded ? 'rounded-md' : ''}
       ${isPrimary ? 'bg-black text-white border-black' : 'border-purple-500 text-black bg-white'}
       font-bold
