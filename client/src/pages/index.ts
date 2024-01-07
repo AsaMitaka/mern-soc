f2a9419cc7729f2674de7cdd4a7e0eb8d52e5477
AsaMitaka <cepelits@gmail.com>
2024-01-07 03:12:06 +0200

    * add user page

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as About } from './about';
 export { default as Home } from './home';
 export { default as Login } from './login';
 export { default as Register } from './register';
+export { default as Profile } from './profile';
