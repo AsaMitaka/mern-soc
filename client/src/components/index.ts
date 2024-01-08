d05ec9dc220a1cbd579f9031b66f15a2f18e521f
AsaMitaka <cepelits@gmail.com>
2024-01-09 00:07:28 +0200

    * added new routes to component root file

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as Header } from './header';
+export { default as HomeLayout } from './homeLayout';
+export { default as NewPost } from './newPost';
 export { default as Post } from './post';
 export { default as profileLayout } from './profileLayout';
+export { default as RightBar } from './rightbar';
 export { default as Trends } from './trends';
-// export { default as  } from './';
