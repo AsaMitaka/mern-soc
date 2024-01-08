93ebed7f1213bc6cd3f35b85ed72599b70c4d2df
AsaMitaka <cepelits@gmail.com>
2024-01-09 00:08:44 +0200

    * added new root to root page file

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -3,6 +3,7 @@ export { default as Error } from './error';
 export { default as Home } from './home';
 export { default as Login } from './login';
 export { default as Register } from './register';
+export { default as Post } from './post';
 export { default as ProfileLikes } from './profileLikes';
 export { default as ProfilePosts } from './profilePosts';
 export { default as Search } from './search';
