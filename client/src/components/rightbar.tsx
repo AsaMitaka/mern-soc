aca73e9ff02b6eda37420ebc298cdc4d7abd4669
AsaMitaka <cepelits@gmail.com>
2024-01-09 00:08:19 +0200

    + rightbar component

---
+++ client/src/components/rightbar.tsx
@@ -0,0 +1,53 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Trends } from '.';
+import Input from './ui/input';
+import { useCallback, useEffect, useState } from 'react';
+
+const RightBar = () => {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [search, setSearch] = useState<string>('');
+
+  useEffect(() => {
+    console.log(searchParams.get('q'));
+    const searchData = searchParams.get('q');
+
+    setSearch(searchData);
+  }, []);
+
+  const handleSearch = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      setSearch(e.target.value);
+    },
+    [setSearch],
+  );
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      if (search.length === 0) return;
+
+      navigate(`/search?q=${search}`);
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="px-4">
+        <Input
+          isBig
+          isRounded
+          onKeyDown={handleKeyDown}
+          onChange={(e) => handleSearch(e)}
+          placeholder="Search"
+          value={search}
+        />
+      </div>
+      <Trends />
+    </div>
+  );
+};
+
+export default RightBar;
