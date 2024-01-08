f9b3818a481c220c296ecc14c0604413914cc618
AsaMitaka <cepelits@gmail.com>
2024-01-09 00:08:29 +0200

    + main page

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,16 +1,23 @@
+import { NewPost } from '../components';
 import Post from '../components/post';
-import Trends from '../components/trends';
 
 const Home = () => {
   return (
-    <div className="flex min-h-svh">
-      <div className="w-2/3 border-l-[1px] border-r-[1px]">
-        <Post />
-      </div>
-      <div className="w-1/3">
-        <Trends />
-      </div>
-    </div>
+    <>
+      <NewPost />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
   );
 };
 
