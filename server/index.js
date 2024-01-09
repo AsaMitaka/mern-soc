7c6b0e5782fcffed82f5e929fcbf9734d13ecb6e
AsaMitaka <cepelits@gmail.com>
2024-01-09 18:56:10 +0200

    * add /assets/ imgs

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 app.use('/api', route);
 
 const start = async () => {
