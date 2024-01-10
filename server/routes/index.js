3bbd5a472af50bec121839328c2e44fc1f2f67c0
AsaMitaka <cepelits@gmail.com>
2024-01-11 00:39:18 +0200

    * add getPosts

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const route = express.Router();
 const authRoute = require('../routes/auth.route');
-const commentRoute = require('../routes/comment.route');
+// const commentRoute = require('../routes/comment.route');
 const postRoute = require('../routes/post.route');
 const userRoute = require('../routes/user.route');
 
 route.use('/', postRoute);
-route.use('/', commentRoute);
+// route.use('/', commentRoute);
 route.use('/', authRoute);
 route.use('/', userRoute);
 
