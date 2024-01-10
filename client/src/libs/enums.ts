26bcf6a88632bb48b32566061b4c0b92b46e56d7
AsaMitaka <cepelits@gmail.com>
2024-01-10 02:56:42 +0200

    + route enums

---
+++ client/src/libs/enums.ts
@@ -0,0 +1,13 @@
+enum AppRoute {
+  LOGIN = '/login',
+  REGISTER = '/register',
+  HOME = '/',
+  POST = '/post/:id',
+  ABOUT = '/about',
+  SEARCH = '/search?',
+  PROFILEPOSTS = '/profile/:id',
+  PROFILELIKES = '/profile/:id/likes',
+  ERROR = '*',
+}
+
+export default AppRoute;
