edb34549e23351995684bb7175b80d0045b6cddd
AsaMitaka <cepelits@gmail.com>
2024-01-11 00:38:12 +0200

    * deleted upload multer, assets path

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const route = require('./routes/');
+const path = require('path');
 require('dotenv').config();
 
 const PORT = 3000;
@@ -11,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+// app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 app.use('/api', route);
 
 const start = async () => {
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const upload = require('../config/multer');
+// const upload = require('../config/multer');
 const route = express.Router();
 const { getUser, updateUser, deleteUser } = require('../controllers/user.controller');
 const verifyToken = require('../middleware/verifyToken');
 
 route.get('/user/:userId/', getUser);
-route.patch('/user/:userId', verifyToken, upload.single('profilePic'), updateUser);
+// route.patch('/user/:userId', verifyToken, upload.single('profilePic'), updateUser);
 route.delete('/user/:userId', verifyToken, deleteUser);
 
 module.exports = route;
