02ad7910b3405676c696f736174dfa37d474db47
AsaMitaka <cepelits@gmail.com>
2024-01-11 00:39:35 +0200

    + upd routes

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -5,14 +5,16 @@ const {
   deletePost,
   editPost,
   getPost,
+  getPosts,
   getUserPosts,
 } = require('../controllers/post.controller');
 const verifyToken = require('../middleware/verifyToken');
 
 route.post('/post', verifyToken, createPost);
-route.get('/user/:userId/posts', getUserPosts);
+route.get('/post/all', getPosts);
 route.get('/post/:postId', getPost);
-route.patch('/post/:postId', verifyToken, editPost);
+route.get('/user/:userId/posts', getUserPosts);
+// route.patch('/post/:postId', verifyToken, editPost);
 route.delete('/post/:postId', verifyToken, deletePost);
 
 module.exports = route;
