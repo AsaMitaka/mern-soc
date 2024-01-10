57703a4ab50b39522f875f466ffbeebadfb941d3
AsaMitaka <cepelits@gmail.com>
2024-01-11 00:48:36 +0200

    * minor upd

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
 import { store } from './store/store.ts';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
