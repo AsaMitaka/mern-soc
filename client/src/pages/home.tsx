5f3e7f2eb33b67aa860755791532eea5191f230f
AsaMitaka <cepelits@gmail.com>
2024-01-11 00:51:34 +0200

    + get posts data

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
 import { NewPost } from '../components';
 import Post from '../components/post';
+import axios from '../libs/axios';
 
 const Home = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const postData = await axios.get('/post/all');
+        console.log(postData);
+
+        setData(postData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getData();
+    console.log(data);
+  }, []);
+
   return (
     <>
       <NewPost />
