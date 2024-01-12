62f678883da24f2a467b14ae75b4f7fc83c21f05
AsaMitaka <cepelits@gmail.com>
2024-01-12 22:41:58 +0200

    + home page add loading

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,42 +1,53 @@
 import { useEffect, useState } from 'react';
-import { NewPost } from '../components';
-import Post from '../components/post';
+import { NewPost, PostComp } from '../components';
 import axios from '../libs/axios';
 
 const Home = () => {
   const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const getData = async () => {
       try {
+        setLoading(true);
         const postData = await axios.get('/post/all');
-        console.log(postData);
 
-        setData(postData);
+        setData(postData.data);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     getData();
-    console.log(data);
   }, []);
 
   return (
-    <>
+    <div className="flex flex-col">
       <NewPost />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </>
+      <div className="flex flex-col">
+        {loading ? (
+          <div className="self-center">
+            <h1 className="text-xl font-bold">Loading</h1>
+          </div>
+        ) : data?.length > 0 ? (
+          data.map((item: any) => (
+            <PostComp
+              id={item._id}
+              key={item._id}
+              text={item.text}
+              authorId={item.authorId}
+              comments={item.comments}
+              data={item.createdAt}
+            />
+          ))
+        ) : (
+          <div className="self-center">
+            <h1 className="text-xl font-bold">Posts empty</h1>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
