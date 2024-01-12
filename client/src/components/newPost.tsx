7cc9dc561414e4162d8228b453dd5bfb9d5be849
AsaMitaka <cepelits@gmail.com>
2024-01-12 22:38:52 +0200

    + newPost with textarea

--- client/src/components/newPost.tsx
+++ client/src/components/newPost.tsx
@@ -1,21 +1,49 @@
-import Btn from './ui/button';
-import Input from './ui/input';
-import pic from '../assets/login.jpg';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { Btn, Textarea } from '.';
+import pic from '../assets/login.jpg';
+import axios from '../libs/axios';
 
 const NewPost = () => {
+  const [text, setText] = useState('');
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    if (text.length < 1 || text.length > 255) {
+      console.error('Text must be more than 1 or less 255 characters');
+      return;
+    }
+
+    try {
+      const post = await axios.post('/post', {
+        text,
+      });
+
+      if (!post) {
+        console.error('Something went wrong');
+      }
+
+      setText('');
+      toast.success('Post successfully');
+    } catch (error) {
+      console.warn(error);
+      toast.error('Post failed');
+    }
+  };
+
   return (
-    <div className="flex flex-col px-5 py-2 border-b-[1px]">
+    <form className="flex flex-col px-5 pb-2 border-b-[1px]" onSubmit={handleSumbit}>
       <div className="flex items-center">
-        <Link to="/profile/1" className="hover:opacity-80">
-          <img src={pic} className="w-10 h-10 mr-2" />
+        <Link to="/profile/1" className="self-start mr-2 hover:opacity-80">
+          <img src={pic} className="w-10 h-10" />
         </Link>
-        <Input isBig isOutline placeholder="New Post" />
+        <Textarea onChange={(e) => setText(e.target.value)} placeholder="New Post" value={text} />
       </div>
       <div className="flex justify-end">
-        <Btn title="New Post" />
+        <Btn title="New Post" type="submit" />
       </div>
-    </div>
+    </form>
   );
 };
 
