92a29de3f57c5115d11d8aea257c3c1357f19255
AsaMitaka <cepelits@gmail.com>
2024-01-12 22:38:28 +0200

    * added new routes

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,7 +1,13 @@
+export { default as Comment } from './comment';
 export { default as Header } from './header';
 export { default as HomeLayout } from './homeLayout';
 export { default as NewPost } from './newPost';
-export { default as Post } from './post';
-export { default as profileLayout } from './profileLayout';
+export { default as PostComp } from './postComp';
+export { default as PostHeader } from './postHeader';
+export { default as ProfileLayout } from './profileLayout';
 export { default as RightBar } from './rightbar';
 export { default as Trends } from './trends';
+
+export { default as Btn } from './ui/button';
+export { default as Input } from './ui/input';
+export { default as Textarea } from './ui/textarea';
