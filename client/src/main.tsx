e1b6e75eae95451c093da9518c314c38cda4ecb1
AsaMitaka <cepelits@gmail.com>
2024-01-12 22:37:10 +0200

    + added toastcontainer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import App from './App.tsx';
 import { store } from './store/store.ts';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
+        <ToastContainer />
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
