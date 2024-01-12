ec6dbd5a9ee263c0ceeb9ed3b496ea7019ae26c8
AsaMitaka <cepelits@gmail.com>
2024-01-12 22:43:37 +0200

    * fix comp imports, styles, added posts

--- client/src/pages/profilePosts.tsx
+++ client/src/pages/profilePosts.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import axios from '../libs/axios';
 import pic from '../assets/login.jpg';
-import Post from '../components/post';
-import { Trends } from '../components';
-import Btn from '../components/ui/button';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../store/store';
+
+import { Btn, PostComp, Trends } from '../components';
+import { AppDispatch, RootState } from '../store/store';
 import { setLogout } from '../store/slices/auth';
 import { UserProps } from '../libs/types';
 
 const ProfilePosts = () => {
   const { id } = useParams();
+  const isUser = useSelector<RootState>((state) => state.auth.user?._id);
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
-  const [data, setData] = useState<null | UserProps>();
+  const [data, setData] = useState<UserProps>();
 
   const handleLogOut = (e) => {
     e.preventDefault();
@@ -30,7 +30,6 @@ const ProfilePosts = () => {
         const userData = await axios.get(`/user/${id}`);
 
         setData(userData.data.user);
-        console.log(userData.data.user);
       } catch (error) {
         console.log(error);
       }
@@ -41,11 +40,11 @@ const ProfilePosts = () => {
 
   return (
     <div className="mt-3 flex mx-auto md:px-0 px-2 md:flex-row flex-col min-h-svh">
-      <div className="md:w-1/5 flex md:flex-col flex-row md:justify-start justify-between text-black dark:text-white">
+      <div className="md:w-1/5 flex md:flex-col flex-row md:justify-start justify-between md:px-2 px-0 text-black dark:text-white">
         <div className="">
           <img src={pic} alt="user pic" className="w-36 h-36 border-white border-3" />
         </div>
-        <div>
+        <div className="">
           <h1 className="text-lg font-bold">{data?.username}</h1>
           <h3>@{data?.name}</h3>
           <h4 className="font-light">Joined {data?.createdAt}</h4>
@@ -74,12 +73,22 @@ const ProfilePosts = () => {
               Likes
             </Link>
           </div>
-          <div className="">
-            <Btn title="Follow" />
-          </div>
+          <div className="">{isUser !== id && <Btn title="Follow" />}</div>
         </div>
         <div className="">
-          <Post />
+          {data.posts.length > 0 ? (
+            data?.posts.map((post) => (
+              <PostComp
+                key={post._id}
+                id={post._id}
+                text={post.text}
+                authorId={data}
+                comments={post.comments}
+              />
+            ))
+          ) : (
+            <h1 className="mt-2 px-4 flex self-center text-xl font-bold">Post empty</h1>
+          )}
         </div>
       </div>
       <div className="md:w-1/5 md:block hidden">
